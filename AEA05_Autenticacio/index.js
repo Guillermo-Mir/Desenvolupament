a92eecd4b083cd1da8251e6e9571593337746394
Guillermo-Mir <guillermo.mir.7e8@itb.cat>
2025-10-07 17:44:47 +0200

    userRepository

--- AEA05_Autenticacio/index.js
+++ AEA05_Autenticacio/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { PORT, SECRET_JWT_KEY } from "./config.js";
 import { useImperativeHandle } from "react";
+import { UserRepository } from "./user-repository.js";
 
 const app = express(); // crear el servidor
 
