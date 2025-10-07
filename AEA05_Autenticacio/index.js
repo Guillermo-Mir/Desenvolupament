1148f94b672fbde251badca3aee8b60d38a2c7a9
Guillermo-Mir <guillermo.mir.7e8@itb.cat>
2025-10-07 18:41:03 +0200

    arreglos2

--- AEA05_Autenticacio/index.js
+++ AEA05_Autenticacio/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { PORT, SECRET_JWT_KEY } from "./config.js";
-//import { useImperativeHandle } from "react";
 import { UserRepository } from "./user-repository.js";
 
 const app = express(); // crear el servidor
