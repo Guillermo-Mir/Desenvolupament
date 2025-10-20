f6686266abe5c4170c08ca775d0354d7c1e47370
Guillermo-Mir <guillermo.mir.7e8@itb.cat>
2025-10-20 19:53:02 +0200

    Create a medias

--- AEA06_Autenticacio_EJS_books&products/routes/pokemons.js
+++ AEA06_Autenticacio_EJS_books&products/routes/pokemons.js
@@ -37,12 +37,21 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    const data = readData();
-    const body = req.body;
-    const newPokemon = { id: data.pokemons.length + 1, ...body };
+     const data = readData();
+    const { name, type, generation } = req.body; 
+    
+    if (!name || !type || !generation) return res.status(400).send('Falten camps obligatoris');
+    
+    const newPokemon = { 
+        id: data.pokemons.length + 1, 
+        name, 
+        type, 
+        generation,
+    };
+    
     data.pokemons.push(newPokemon);
     writeData(data);
-    res.json(newPokemon);
+    res.redirect('/pokemons');
 });
 
 router.put('/:id', (req, res) => {
@@ -62,7 +71,7 @@ router.delete('/:id', (req, res) => {
     if (pokemonIndex === -1) return res.status(404).send('Pokemon not found');
     data.pokemons.splice(pokemonIndex, 1);
     writeData(data);
-    res.redirect("/pokemons")
+    res.redirect('/pokemons')
     //res.json({ message: "Pokemon deleted successfully" });
 });
 
